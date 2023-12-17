6d3362a22955be3d8545a51f92ff12365cd31dae
YxuSinX <mewhenotreal@gmail.com>
2023-12-17 13:23:39 +0800

    Update verify.js

--- js/verify.js
+++ js/verify.js
@@ -1,17 +1,10 @@
-// verify.js
-
-// Delay in milliseconds before redirection
-const delay = 1000; // 3 seconds
-
-// URL to redirect to
+const delay = 1000;
 const redirectUrl = `https://restorecord.com/verify/Yxu%20Sin's%20Community`;
 
-// Function to redirect after a delay
 function redirectToUrl() {
     setTimeout(() => {
         window.location.href = redirectUrl;
     }, delay);
 }
 
-// Call the redirection function
 redirectToUrl();
