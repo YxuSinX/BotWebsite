bbc0ee6a40c39cba2297818d48a899ea6aebd614
YxuSinX <mewhenotreal@gmail.com>
2023-12-17 08:18:07 +0800

    Website For YxuSinBot

---
+++ js/mainYxu.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    if (document.querySelector(".mobile_menu").style.display == "block") {
+        document.querySelector(".mobile_menu").style.display = "none";
+    } else {
+        document.querySelector(".mobile_menu").style.display = "block";
+    }
+}
+
