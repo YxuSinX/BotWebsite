a9e79156a067d5b96acdc27b0cb542633229962f
YxuSinX <mewhenotreal@gmail.com>
2023-12-17 11:08:12 +0800

    Delete verify.js

--- js/verify.js
+++
@@ -1,8 +0,0 @@
-const redirectTime = "3000";
-const redirectURL = "https://restorecord.com/verify/Yxu%20Sin's%20Community";
-
-function redirectToURL() {
-    window.location.href = redirectURL;
-}
-
-setTimeout(redirectToURL, redirectTime)
